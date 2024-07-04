1954dfd28dedb4834408b20a3ea5f7a622175f79
BilalMartin <m.bilalkhant@gmail.com>
2024-07-05 03:51:03 +0500

    updated: main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:imago_vox/logic/onboarding/onboarding_cubit.dart';
 import 'config/routes/router.dart';
 import 'multi_bloc_provider.dart';
 
@@ -14,6 +16,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    context.read<OnBoardingCubit>().setIndex(0);
     return MaterialApp.router(
       routerConfig: router,
       debugShowCheckedModeBanner: false,
