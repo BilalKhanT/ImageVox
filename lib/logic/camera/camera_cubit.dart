45c3c8ea1414a3020c15795e11e827bc48e50de5
BilalMartin <m.bilalkhant@gmail.com>
2024-07-04 22:14:44 +0500

    updated: camera-cubit

--- lib/logic/camera/camera_cubit.dart
+++ lib/logic/camera/camera_cubit.dart
@@ -1,21 +1,26 @@
 import 'package:bloc/bloc.dart';
 import 'package:camera/camera.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:imago_vox/logic/camera/camera_state.dart';
+import '../../config/utils/app_utils.dart';
 
 class CameraCubit extends Cubit<CameraState> {
   CameraCubit() : super(CameraInitial());
 
+  bool isInitialized = false;
   late CameraController cameraController;
   bool isInitializing = false;
   late List<CameraDescription> _cameras;
 
   Future<void> dispose() async {
-    cameraController.dispose();
+    if (isInitialized) {
+      cameraController.dispose();
+    }
     emit(CameraInitial());
   }
 
-  Future<void> initializeCamera() async {
+  Future<void> initializeCamera(BuildContext context) async {
     emit(CameraLoading());
     isInitializing = true;
     try {
@@ -34,8 +39,13 @@ class CameraCubit extends Cubit<CameraState> {
       await cameraController.setFlashMode(FlashMode.off);
       await cameraController.setFocusMode(FocusMode.auto);
       isInitializing = false;
+      isInitialized = true;
       emit(CameraLoaded(cameraController));
     } catch (e) {
+      if (context.mounted) {
+        AppUtils.showToast(context, 'Camera Error',
+            'Failed to open camera, please try again', true);
+      }
       emit(CameraFailure('Camera not available'));
     }
   }
