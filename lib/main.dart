4e645e73d7fea404cce2317cd0b2e4559c34a541
BilalMartin <m.bilalkhant@gmail.com>
2024-07-05 04:09:20 +0500

    added: shared-pref initialization in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:imago_vox/logic/onboarding/onboarding_cubit.dart';
+import 'package:nb_utils/nb_utils.dart';
 import 'config/routes/router.dart';
+import 'data/local/shared_pref.dart';
 import 'multi_bloc_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeApp();
   runApp(
     const ProvideMultiBloc(child: MyApp()),
   );
+}
+
+Future<void> initializeApp() async {
+  await sharedPrefs.init();
+  await initialize();
 }
 
 class MyApp extends StatelessWidget {
