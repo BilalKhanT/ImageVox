cabba880369d25c9c750759c43e4801a89f937f3
BilalMartin <m.bilalkhant@gmail.com>
2024-07-04 22:13:54 +0500

    updated: image-capture ui

--- lib/presentation/image/image_capture_view.dart
+++ lib/presentation/image/image_capture_view.dart
@@ -1,9 +1,10 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:imago_vox/config/utils/app_colors.dart';
-import 'package:imago_vox/config/utils/app_utils.dart';
 import 'package:imago_vox/logic/camera/camera_state.dart';
 import 'package:imago_vox/presentation/widgets/dot_loader.dart';
 import '../../config/routes/route_names.dart';
@@ -21,6 +22,7 @@ class ImageCaptureView extends StatelessWidget {
       child: Scaffold(
         backgroundColor: AppColors.appColor,
         appBar: AppBar(
+          backgroundColor: AppColors.appColor,
           leading: IconButton(
             onPressed: () async {
               await context.read<CameraCubit>().dispose();
@@ -28,19 +30,13 @@ class ImageCaptureView extends StatelessWidget {
                 context.go(RouteNames.homeRoute);
               }
             },
-            icon: const Icon(Icons.arrow_back_ios),
+            icon: const Icon(Icons.arrow_back_ios, color: Colors.white,),
           ),
         ),
         body: SizedBox(
           height: height,
           width: width,
-          child: BlocConsumer<CameraCubit, CameraState>(
-            listener: (context, state) {
-              if (state is CameraFailure) {
-                AppUtils.showToast(context, 'Camera Error',
-                    'Failed to open camera, please try again', true);
-              }
-            },
+          child: BlocBuilder<CameraCubit, CameraState>(
             builder: (context, state) {
               if (state is CameraLoading) {
                 return const Center(
@@ -48,6 +44,9 @@ class ImageCaptureView extends StatelessWidget {
                     loaderColor: Colors.white,
                   ),
                 );
+              } else if (state is CameraFailure) {
+                context.go(RouteNames.homeRoute);
+                return const SizedBox.shrink();
               } else if (state is CameraLoaded) {
                 CameraController camera = state.camera;
                 return camera.value.isInitialized == true
@@ -80,7 +79,7 @@ class ImageCaptureView extends StatelessWidget {
                                       decoration: BoxDecoration(
                                         borderRadius: BorderRadius.circular(
                                             height * 0.04),
-                                        color: Colors.white,
+                                        color: AppColors.btnColor,
                                         boxShadow: [
                                           BoxShadow(
                                             offset: const Offset(1, 3),
@@ -103,7 +102,42 @@ class ImageCaptureView extends StatelessWidget {
                           color: AppColors.appColor,
                         ),
                       );
-              } else {
+              } else if (state is CameraCaptured) {
+                return Stack(
+                  children: [
+                    SizedBox(
+                      height: height,
+                      width: width,
+                      child: Image.file(
+                        File(state.image.filePath.toString()),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 25,
+                      left: width * 0.1,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () async {
+                              await context.read<CameraCubit>().initializeCamera(context);
+                            },
+                            child: Text('Recapture'),
+                          ),
+                          SizedBox(width: 20),
+                          ElevatedButton(
+                            onPressed: () {
+                              // Handle proceed action
+                            },
+                            child: Text('Proceed'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              }else {
                 return const SizedBox.shrink();
               }
             },
