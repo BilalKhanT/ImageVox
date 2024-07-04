75ffe01bd90a023ef854eeb85ad3eecc4621c1b7
BilalMartin <m.bilalkhant@gmail.com>
2024-07-04 22:33:00 +0500

    updated: ui and methods

--- lib/presentation/home/home_view.dart
+++ lib/presentation/home/home_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:imago_vox/config/routes/route_names.dart';
 import 'package:imago_vox/config/utils/app_colors.dart';
+import 'package:imago_vox/config/utils/app_utils.dart';
 import 'package:imago_vox/logic/camera/camera_cubit.dart';
 import 'package:imago_vox/presentation/widgets/cstm_btn.dart';
 import 'package:lottie/lottie.dart';
@@ -37,7 +39,7 @@ class HomeView extends StatelessWidget {
                   fontFamily: 'MontserratMedium',
                   fontSize: width * 0.07,
                   fontWeight: FontWeight.w800,
-                  color: Colors.white.withOpacity(0.8),
+                  color: Colors.white.withOpacity(0.9),
                   letterSpacing: 1.0,
                 ),
               ),
@@ -45,21 +47,42 @@ class HomeView extends StatelessWidget {
                 height: height * 0.05,
               ),
               CustomButton(
-                  onTap: () {
-                    context.read<CameraCubit>().initializeCamera(context);
-                    context.go(RouteNames.imgCaptureRoute);
-                  },
-                  text: 'Capture Image',
-                  btnColor: AppColors.btnColor,
-                  width: width),
+                onTap: () {
+                  context.read<CameraCubit>().initializeCamera(context);
+                  context.go(RouteNames.imgCaptureRoute);
+                },
+                text: 'Capture Image',
+                btnColor: AppColors.btnColor,
+                width: width,
+                svgPath: 'assets/svgs/camera.svg',
+              ),
               SizedBox(
                 height: height * 0.03,
               ),
               CustomButton(
-                  onTap: () {},
-                  text: 'Upload Image',
-                  btnColor: AppColors.btnColor,
-                  width: width),
+                onTap: () async {
+                  final pickedFile =
+                  await ImagePicker().pickImage(
+                    source: ImageSource.gallery,
+                    maxWidth: 1800,
+                    maxHeight: 1800,
+                    imageQuality: 85,
+                  );
+                  if (context.mounted) {
+                    if (pickedFile != null) {
+                      context.read<CameraCubit>().uploadImage(pickedFile);
+                      context.go(RouteNames.imgCaptureRoute);
+                    }
+                    else {
+                      AppUtils.showToast(context, 'Image Error', 'Failed to open image, please try again', true);
+                    }
+                  }
+                },
+                text: 'Upload Image',
+                btnColor: AppColors.btnColor,
+                width: width,
+                svgPath: 'assets/svgs/gallery.svg',
+              ),
             ],
           ),
         ),
